a74d3fc50e90d126a922d0bb0c632549c141dd0e
zmmm2 <1895145@qq.com>
2026-02-07 18:12:06 +0800

    Delete random.php

--- random.php
+++
@@ -1,9 +0,0 @@
-<?php
-$route="admin/random";
-$str=file_get_contents($route);
-$str=(explode("|",$str));
-$arraynum=count($str);
-$arraynum=$arraynum-1;
-$num=rand(0,$arraynum);
-echo $str[$num];
-?>
