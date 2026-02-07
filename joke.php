e188acf5e10b501a6631287645f99e225efdf09a
zmmm2 <1895145@qq.com>
2026-02-07 18:11:10 +0800

    Delete joke.php

--- joke.php
+++
@@ -1,9 +0,0 @@
-<?php
-$route="admin/joke";
-$str=file_get_contents($route);
-$str=(explode("|",$str));
-$arraynum=count($str);
-$arraynum=$arraynum-1;
-$num=rand(0,$arraynum);
-echo $str[$num];
-?>
